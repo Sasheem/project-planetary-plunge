c64b2b5819230b7c8f491c8538e91ee3dae10585
Sasheem <sasheem.snell@gmail.com>
2023-09-20 13:33:00 -0400

    Added ballista towers that target the enemy

--- Assets/Tiles/Waypoint.cs
+++ Assets/Tiles/Waypoint.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class Waypoint : MonoBehaviour
 {
+    [SerializeField] GameObject towerPrefab;
     [SerializeField] bool isPlaceable;
 
     void OnMouseDown() {
         if (isPlaceable) {
-            Debug.Log(transform.name);
+            Instantiate(towerPrefab, transform.position, Quaternion.identity);
+            isPlaceable = false;
         }
     }
 }
