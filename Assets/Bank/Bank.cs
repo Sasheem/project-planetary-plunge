e1716692a1eee825037dd2ba32d0b3486d58381d
Sasheem <sasheem.snell@gmail.com>
2023-09-25 20:32:13 -0400

    UI text to show running total of gold

--- Assets/Bank/Bank.cs
+++ Assets/Bank/Bank.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
 
 public class Bank : MonoBehaviour
 {
@@ -8,15 +10,34 @@ public class Bank : MonoBehaviour
     [SerializeField] int currentBalance;
     public int CurrentBalance { get { return currentBalance; } }
 
+    [SerializeField] TextMeshProUGUI displayBalance;
+
     void Awake() {
         currentBalance = startingBalance;
     }
 
     public void Deposit(int amount) {
         currentBalance += Mathf.Abs(amount);
+        updateDisplay();
     }
 
     public void Withdraw(int amount) {
         currentBalance -= Mathf.Abs(amount);
+        updateDisplay();
+
+        if (currentBalance < 0) {
+            // production - call in to another method or scene manager to handle end of level
+            // lose the game condition
+            ReloadScene();
+        }
+    }
+
+    void updateDisplay() {
+        displayBalance.text = "Gold: " + currentBalance;
+    }
+
+    void ReloadScene() {
+        Scene currentScene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(currentScene.buildIndex);
     }
 }
