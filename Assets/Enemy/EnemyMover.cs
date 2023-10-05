80cee7726e9536d3f6a8caa9334af4848e9bbe2f
Sasheem <sasheem.snell@gmail.com>
2023-10-05 14:23:57 -0400

    Find path dynamically with breadth first search and block tiles at start

--- Assets/Enemy/EnemyMover.cs
+++ Assets/Enemy/EnemyMover.cs
@@ -7,7 +7,7 @@ public class EnemyMover : MonoBehaviour
 {
     // pass in some waypoints to follow
     // loop through list and print to console
-    [SerializeField] List<Waypoint> path = new List<Waypoint>();
+    [SerializeField] List<Tile> path = new List<Tile>();
     [SerializeField] [Range(0f, 5f)] float speed = 1f;
 
     Enemy enemy;
@@ -32,9 +32,9 @@ public class EnemyMover : MonoBehaviour
 
         // loop through each child of the GameObject Path and add it to the list 
         foreach (Transform child in parent.transform) {
-            Waypoint waypoint = child.GetComponent<Waypoint>();
-            if (waypoint != null) {
-                path.Add(waypoint);
+            Tile tile = child.GetComponent<Tile>();
+            if (tile != null) {
+                path.Add(tile);
             }
         }
     }
@@ -51,10 +51,10 @@ public class EnemyMover : MonoBehaviour
 
     // loop through the list and print names
     IEnumerator FollowPath() {
-        foreach(Waypoint waypoint in path) {
+        foreach(Tile tile in path) {
             // set up start and end position we want to move to
             Vector3 startPosition = transform.position;
-            Vector3 endPosition = waypoint.transform.position;
+            Vector3 endPosition = tile.transform.position;
             float travelPercent = 0f;
 
             // always facing the waypoint enemy is heading to
---
+++ Assets/Pathfinding/Pathfinder.cs
@@ -0,0 +1,99 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pathfinder : MonoBehaviour
+{
+    [SerializeField] Vector2Int startCoordinates;
+    [SerializeField] Vector2Int destinationCoordinates;
+    Node startNode;
+    Node destinationNode;
+    Node currentSearchNode;
+
+    Queue<Node> frontier = new Queue<Node>();
+    Dictionary<Vector2Int, Node> reached = new Dictionary<Vector2Int, Node>();       // keep track of explored nodes
+
+    Vector2Int[] directions = { Vector2Int.right, Vector2Int.left, Vector2Int.up, Vector2Int.down };
+    GridManager gridManager;
+    Dictionary<Vector2Int, Node> grid = new Dictionary<Vector2Int, Node>();
+
+    void Awake() {
+        gridManager = FindObjectOfType<GridManager>();
+        if (gridManager != null) {
+            grid = gridManager.Grid;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        startNode = gridManager.Grid[startCoordinates];
+        destinationNode = gridManager.Grid[destinationCoordinates];
+
+        BreadthFirstSearch();
+        BuildPath();
+    }
+
+    void ExploreNeighbors() {
+        // 1 create an empty list called 'neighhbors'
+        List<Node> neighbors = new List<Node>();
+        // 2 loop through all for directions
+        foreach (Vector2Int direction in directions) {
+            // 3 calculate the coordinates of the node in that direction from our currentSearchNode
+            Vector2Int neighborCoods = currentSearchNode.coordinates + direction;
+
+            // 4 check if the neighbor's coordinates exist in the grid
+            if (grid.ContainsKey(neighborCoods)) {
+                // 5 if it does exist in the grid, add it to our neighbors list
+                neighbors.Add(grid[neighborCoods]);
+            }
+        }
+
+        // loop through neighbors
+        foreach (Node neighbor in neighbors) {
+            // check if Dictionary of reached nodes has this current neighbor
+            // check if neighbor is walkable
+            if (!reached.ContainsKey(neighbor.coordinates) && neighbor.isWalkable) {
+                // creates the connection between nodes to build tree
+                neighbor.connectedTo = currentSearchNode;
+                // keep track of what we've reached
+                reached.Add(neighbor.coordinates, neighbor);
+                // keep track to potentially add to final path?
+                frontier.Enqueue(neighbor);
+            }
+        }
+    }
+
+    void BreadthFirstSearch () {
+        bool isRunning = true;
+
+        frontier.Enqueue(startNode);
+        reached.Add(startCoordinates, startNode);
+
+        while(frontier.Count > 0 && isRunning) {
+            currentSearchNode = frontier.Dequeue();
+            currentSearchNode.isExplored = true;
+            ExploreNeighbors();
+            if (currentSearchNode.coordinates == destinationCoordinates) {
+                isRunning = false;
+            }
+        }
+    }
+
+    List<Node> BuildPath() {
+        List<Node> path = new List<Node>();
+        Node currentNode = destinationNode;
+
+        path.Add(currentNode);
+        currentNode.isPath = true;
+
+        while (currentNode.connectedTo != null) {
+            currentNode = currentNode.connectedTo;
+            path.Add(currentNode);
+            currentNode.isPath = true;
+        }
+
+        path.Reverse();
+
+        return path;
+    }
+}
