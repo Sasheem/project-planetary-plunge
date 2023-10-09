ff53ecafc0c3efd6e1b145484bcdc682c94452ae
Sasheem <sasheem.snell@gmail.com>
2023-10-09 11:26:21 -0400

    Added build delay to ballista towers

--- Assets/Tower/Tower.cs
+++ Assets/Tower/Tower.cs
@@ -5,6 +5,12 @@ using UnityEngine;
 public class Tower : MonoBehaviour
 {
     [SerializeField] int cost = 75;
+    [SerializeField] float buildDelay = 1f;
+
+    void Start() {
+        StartCoroutine(Build());
+    }
+
     public bool CreateTower(Tower tower, Vector3 position) {
         Bank bank = FindObjectOfType<Bank>();
 
@@ -19,5 +25,26 @@ public class Tower : MonoBehaviour
         }
 
         return false;
+    }
+
+    // Add a build delay
+    IEnumerator Build() {
+        // turn off all children and grandchildren in the hiearcrhy 
+        foreach(Transform child in transform) {
+            child.gameObject.SetActive(false);
+            foreach(Transform grandchild in child) {
+                grandchild.gameObject.SetActive(false);
+            }
+        }
+        // enable the children and grandchildren sequentially
+        foreach(Transform child in transform) {
+            child.gameObject.SetActive(true);
+
+            // Add a build delay to control how quickly they become active
+            yield return new WaitForSeconds(buildDelay);
+            foreach(Transform grandchild in child) {
+                grandchild.gameObject.SetActive(true);
+            }
+        }
     }
 }
